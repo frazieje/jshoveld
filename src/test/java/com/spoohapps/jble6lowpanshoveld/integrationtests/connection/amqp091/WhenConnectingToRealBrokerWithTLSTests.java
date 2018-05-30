70eca43034a7b33e77fa96327f1952e83cf95a32
Joel Frazier <frazieje@gmail.com>
2018-05-30 04:17:20 -0400

    integration tests for broker connections

--- src/test/java/com/spoohapps/jble6lowpanshoveld/integrationtests/connection/amqp091/WhenConnectingToRealBrokerWithTLSTests.java
+++ src/test/java/com/spoohapps/jble6lowpanshoveld/integrationtests/connection/amqp091/WhenConnectingToRealBrokerWithTLSTests.java
@@ -14,15 +14,22 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class WhenConnectingToRealBroker {
-
+public class WhenConnectingToRealBrokerWithTLSTests {
     private ConnectionFactory factory;
 
     private PublisherConnection publisherConnection;
@@ -34,15 +41,38 @@ public class WhenConnectingToRealBroker {
     private final String testTopic = "test.topic";
 
     @BeforeAll
-    public void setup() {
+    public void setup() throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
         testExecutor = Executors.newFixedThreadPool(3);
+
+        char[] keyPassphrase = "somepassword".toCharArray();
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+
+        Path pkcs12Path = Paths.get(System.getProperty("user.home"), "jble6lowpanshoveld", "client", "spoohappsmqclient.p12");
+        Path trustStorePath = Paths.get(System.getProperty("user.home"), "jble6lowpanshoveld", "client", "spoohappsmqca.jks");
+
+        ks.load(Files.newInputStream(pkcs12Path), keyPassphrase);
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, keyPassphrase);
+
+        char[] trustPassphrase = "somepassword".toCharArray();
+        KeyStore tks = KeyStore.getInstance("JKS");
+        tks.load(Files.newInputStream(trustStorePath), trustPassphrase);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(tks);
+
+        SSLContext c = SSLContext.getInstance("TLSv1.2");
+        c.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
         factory = new Amqp091ConnectionFactory(
                 new RabbitMqAmqp091ConnectionSupplier(
                         testExecutor,
                         "www.spoohapps.com",
-                        5672,
-                        "somepassphrase",
-                        "somepassphrase"
+                        5671,
+                        "jble6lowpanshoveld",
+                        "jble6lowpanshoveld",
+                        c
                 ));
 
         ConnectionSettings publisherSettings = new Amqp091PublisherConnectionSettings("TestExchange");
@@ -81,9 +111,32 @@ public class WhenConnectingToRealBroker {
 
     @Test
     public void shouldConsumeMessage() {
-        publisherConnection.publish(new Message(testTopic, new byte[] { 1, 2 }));
-        sleep(3000);
+        publisherConnection.publish(new Message(testTopic, new byte[] { 1, 4 }));
+        sleep(1500);
         assertNotNull(receivedMessage);
+    }
+
+    @Test
+    public void shouldConsumeMessageWithSameTopic() {
+        publisherConnection.publish(new Message(testTopic, new byte[] { 1, 2 }));
+        sleep(1500);
+        assertEquals(testTopic, receivedMessage.getTopic());
+    }
+
+    @Test
+    public void shouldConsumeMessageWithSamePayload() {
+        byte[] expectedPayload = new byte[] { 1, 3 };
+        publisherConnection.publish(new Message(testTopic, new byte[] { 1, 3 }));
+        sleep(1500);
+        assertArrayEquals(expectedPayload, receivedMessage.getPayload());
+    }
+
+    @Test
+    public void shouldConsumeMessageWithSameHops() {
+        int expectedHops = 1;
+        publisherConnection.publish(new Message(testTopic, expectedHops, new byte[] { 1, 3 }));
+        sleep(1500);
+        assertEquals(expectedHops, receivedMessage.getHops());
     }
 
     private void sleep(int timeMs) {
@@ -93,5 +146,4 @@ public class WhenConnectingToRealBroker {
             e.printStackTrace();
         }
     }
-
 }
