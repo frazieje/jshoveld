ffdadf2647fd192c776f84a4e90657c5cb3daa7b
Joel Frazier <frazieje@gmail.com>
2018-06-17 21:02:21 -0400

    remote interface methods must have throws clause

--- src/main/java/com/spoohapps/jble6lowpanshoveld/ShovelDaemonController.java
+++ src/main/java/com/spoohapps/jble6lowpanshoveld/ShovelDaemonController.java
@@ -3,11 +3,12 @@ package com.spoohapps.jble6lowpanshoveld;
 import com.spoohapps.jble6lowpanshoveld.model.Profile;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface ShovelDaemonController extends Remote {
-    List<String> shovelDescriptors();
-    void restartShovels();
-    Profile getProfile();
-    void setProfile(Profile p);
+    List<String> shovelDescriptors() throws RemoteException;
+    void restartShovels() throws RemoteException;
+    Profile getProfile() throws RemoteException;
+    void setProfile(Profile p) throws RemoteException;
 }
